fb379f4b87ac4c3d7d9a5310667fc40bfd706b20
Code Fox <playhadde@gmail.com>
2018-11-10 18:00:28 +0100

    added Record Counter and Generation Counter

--- Assets/de/chojo/WayFinder/util/QMatrixMemory.cs
+++ Assets/de/chojo/WayFinder/util/QMatrixMemory.cs
@@ -3,19 +3,21 @@ using UnityEngine;
 
 namespace de.chojo.WayFinder.util {
     public class QMatrixMemory {
-        
-
         public Point[,] QMatrix { get; set; }
 
         public int X { get; private set; }
 
         public int Y { get; private set; }
 
+        private int _records = 0;
+        private int _generations;
+
         public bool Equals(int x, int y) {
             return X == x && Y == y;
         }
-        
+
         public QMatrixMemory(Vector2Int goal) {
+            Generations = 1;
             QMatrix = new Point[Field.GetInstance().Dimensions.x, Field.GetInstance().Dimensions.y];
             X = goal.x;
             Y = goal.y;
@@ -23,6 +25,19 @@ namespace de.chojo.WayFinder.util {
             for (var i = 0; i < QMatrix.GetLength(0); i++) {
                 for (var j = 0; j < QMatrix.GetLength(1); j++) QMatrix[i, j] = new Point();
             }
+        }
+
+        public void RecordsCountAdd(int count) {
+            _records += count;
+        }
+
+        public int Generations {
+            get { return _generations; }
+            set { _generations = value; }
+        }
+
+        public int Records {
+            get { return _records; }
         }
     }
 }
