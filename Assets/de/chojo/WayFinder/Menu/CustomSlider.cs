6841f6d14690775fe51be56fa63870852600bbd4
Code Fox <playhadde@gmail.com>
2018-11-11 19:17:36 +0100

    removed not used class

--- Assets/de/chojo/WayFinder/Menu/CustomSlider.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.UI;
-
-namespace de.chojo.WayFinder.Menu {
-    [AddComponentMenu("UI/CustomSlider", 34)]
-    public class CustomSlider : Slider {
-        [SerializeField] private Text _text;
-    }
-}
