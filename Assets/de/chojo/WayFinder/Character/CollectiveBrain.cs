49025871ef52dd405866ce4cc0806d2bef6e38aa
Code Fox <playhadde@gmail.com>
2018-11-13 12:52:17 +0100

    added Method to get the highest values of visits and way value

--- Assets/de/chojo/WayFinder/Character/CollectiveBrain.cs
+++ Assets/de/chojo/WayFinder/Character/CollectiveBrain.cs
@@ -17,21 +17,52 @@ namespace de.chojo.WayFinder.Character {
         /// Returns a matrix for a specific goal, if the goal is known
         /// </summary>
         /// <param name="goal"></param>
+        /// <param name="highestVisitValue"></param>
+        /// <param name="highestWayValue"></param>
         /// <returns></returns>
-        public QMatrixMemory FindQMatrix(Vector2Int goal) {
-            foreach(var matrix in _memory)
-                if(matrix.Equals(goal.x, goal.y))
+        public QMatrixMemory FindQMatrix(Vector2Int goal, out BigInteger highestVisitValue, out double highestWayValue) {
+            highestVisitValue = 0;
+            highestWayValue = 0;
+            foreach (var matrix in _memory) {
+                if (matrix.Equals(goal.x, goal.y)) {
+                    highestVisitValue = 0;
+                    highestWayValue = 0;
+
+                    for (var i = 0; i < matrix.QMatrix.GetLength(0); i++) {
+                        for (var j = 0; j < matrix.QMatrix.GetLength(1); j++) {
+                            var wayValue = matrix.QMatrix[i, j].GetBestValue();
+                            if (wayValue > highestWayValue) highestWayValue = wayValue;
+
+                            var visitValue = matrix.QMatrix[i, j].Visits;
+                            if (visitValue > highestVisitValue) highestVisitValue = visitValue;
+                        }
+                    }
+
                     return matrix;
+                }
+            }
+
             return new QMatrixMemory(goal);
         }
+        
+        public QMatrixMemory FindQMatrix(Vector2Int goal) {
+            foreach (var matrix in _memory) {
+                if (matrix.Equals(goal.x, goal.y)) {
+                    return matrix;
+                }
+            }
+
+            return new QMatrixMemory(goal);
+        }
+
 
         /// <summary>
         /// Saves the Data.
         /// </summary>
         /// <param name="newMatrix"></param>
         private void SafeQMatrix(QMatrixMemory newMatrix) {
-            foreach(var matrix in _memory)
-                if(matrix.Equals(newMatrix.X, newMatrix.Y)) {
+            foreach (var matrix in _memory)
+                if (matrix.Equals(newMatrix.X, newMatrix.Y)) {
                     matrix.QMatrix = newMatrix.QMatrix;
                     return;
                 }
@@ -44,7 +75,7 @@ namespace de.chojo.WayFinder.Character {
         /// </summary>
         /// <param name="qMatrixMemory"></param>
         public void CollectData(QMatrixMemory qMatrixMemory) {
-            if(_collectedMemories == null) _collectedMemories = new List<QMatrixMemory>();
+            if (_collectedMemories == null) _collectedMemories = new List<QMatrixMemory>();
             _collectedMemories.Add(qMatrixMemory);
         }
 
@@ -53,8 +84,8 @@ namespace de.chojo.WayFinder.Character {
         /// Merge the collected data and safe them.
         /// </summary>
         public void MergeAndSaveQMatrixData() {
-            if(_collectedMemories == null) return;
-            
+            if (_collectedMemories == null) return;
+
 
             Debug.Log("Merged " + _collectedMemories.Count + " collected Memories!");
 
@@ -64,21 +95,21 @@ namespace de.chojo.WayFinder.Character {
             data.RecordsCountAdd(_collectedMemories[0].Records + CollectedMemories.Count);
             data.Generations = _collectedMemories[0].Generations++;
 
-            for(var i = 0; i < _collectedMemories[0].QMatrix.GetLength(0); i++) {
-                for(var j = 0; j < _collectedMemories[0].QMatrix.GetLength(1); j++) {
+            for (var i = 0; i < _collectedMemories[0].QMatrix.GetLength(0); i++) {
+                for (var j = 0; j < _collectedMemories[0].QMatrix.GetLength(1); j++) {
                     var up = new List<double>();
                     var down = new List<double>();
                     var right = new List<double>();
                     var left = new List<double>();
                     BigInteger visits = BigInteger.Zero;
 
-                    foreach(var player in _collectedMemories) {
+                    foreach (var player in _collectedMemories) {
                         up.Add(player.QMatrix[i, j].GetValue(Directions.Up));
                         down.Add(player.QMatrix[i, j].GetValue(Directions.Down));
                         right.Add(player.QMatrix[i, j].GetValue(Directions.Right));
                         left.Add(player.QMatrix[i, j].GetValue(Directions.Left));
-                        visits = BigInteger.Add(visits, (BigInteger.Subtract(player.QMatrix[i, j].Visits, baseMemory.QMatrix[i, j].Visits)));
-                            
+                        visits = BigInteger.Add(visits,
+                            (BigInteger.Subtract(player.QMatrix[i, j].Visits, baseMemory.QMatrix[i, j].Visits)));
                     }
 
 
@@ -86,7 +117,7 @@ namespace de.chojo.WayFinder.Character {
                     data.QMatrix[i, j].SetValue(Directions.Down, Helper.GetMax(down));
                     data.QMatrix[i, j].SetValue(Directions.Right, Helper.GetMax(right));
                     data.QMatrix[i, j].SetValue(Directions.Left, Helper.GetMax(left));
-                    data.QMatrix[i, j].Visits = BigInteger.Add(visits, data.QMatrix[i,j].Visits);
+                    data.QMatrix[i, j].Visits = BigInteger.Add(visits, baseMemory.QMatrix[i, j].Visits);
                 }
             }
 
@@ -95,7 +126,7 @@ namespace de.chojo.WayFinder.Character {
         }
 
         public List<QMatrixMemory> CollectedMemories {
-            get {return _collectedMemories;}
+            get { return _collectedMemories; }
         }
     }
 }
